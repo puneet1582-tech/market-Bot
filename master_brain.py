17e2c50daa0d10906fa936b7fe195bc1bbbed0ae
puneet1582-tech <puneet1582@gmail.com>
2026-01-29 00:36:30 +0530

    Add stock selection and explanation logic

--- master_brain.py
+++ master_brain.py
@@ -1,1 +1,24 @@
 
+from mode_brain import decide_mode, market_conditions
+from stock_selector import select_stocks
+from fundamental_brain import fundamental_data
+from explanation_brain import explain_mode, explain_stock
+
+# 1. Decide Mode
+mode = decide_mode(market_conditions)
+print("ACTIVE MODE:", mode)
+
+# 2. Select Stocks based on mode
+selected_stocks = select_stocks(mode)
+print("SELECTED STOCKS:", selected_stocks)
+
+# 3. Explain Mode
+mode_explanation = explain_mode(market_conditions, mode)
+print(mode_explanation)
+
+# 4. Explain each selected stock
+for stock in selected_stocks:
+    data = fundamental_data.get(stock, {})
+    if data:
+        stock_explanation = explain_stock(stock, data, mode)
+        print(stock_explanation)
