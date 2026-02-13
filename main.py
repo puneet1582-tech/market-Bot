4e216be143b1e53a7d987307c6de8cb3cc6e400d
puneet1582-tech <puneet1582@gmail.com>
2026-02-13 09:04:23 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from engines.final_report_engine import FinalReportEngine
+import time
 
 engine = FinalReportEngine()
 report = engine.generate_report("RELIANCE", "Energy")
@@ -8,3 +9,7 @@ with open("daily_report.txt", "w") as f:
         f.write(f"{k} : {v}\n")
 
 print("DAILY REPORT GENERATED -> daily_report.txt")
+
+# keep program running (required for Render Web Service)
+while True:
+    time.sleep(60)
