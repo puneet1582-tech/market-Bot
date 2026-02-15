66a9bfbf2b81f52ff60d790d82604f95b9cf7437
puneet1582-tech <puneet1582@gmail.com>
2026-02-15 08:11:58 +0000

    STEP2 FINAL NSE LIVE ENGINE

--- brain_engine.py
+++ brain_engine.py
@@ -1,13 +1,16 @@
 from step2_full_data_engine import FullDataEngine
 
 class BrainEngine:
+
     def __init__(self):
         self.data_engine = FullDataEngine()
 
     def analyze_stock(self, symbol):
         data = self.data_engine.fetch_full_dataset(symbol)
+
         return {
             "symbol": data["symbol"],
-            "records_price": len(data["price_data"]),
+            "price": data["price"],
+            "volume": data["volume"],
             "timestamp": data["timestamp"]
         }
