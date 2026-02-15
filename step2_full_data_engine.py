d6b2afcbbd730f0095c75d48ff219861ab6dc550
puneet1582-tech <puneet1582@gmail.com>
2026-02-15 08:49:15 +0000

    FINAL NSE JSON FIX

--- step2_full_data_engine.py
+++ step2_full_data_engine.py
@@ -6,21 +6,36 @@ class FullDataEngine:
     def __init__(self):
         self.timestamp = datetime.now()
         self.session = requests.Session()
-        self.headers = {"User-Agent": "Mozilla/5.0"}
+        self.headers = {
+            "User-Agent": "Mozilla/5.0",
+            "Accept-Language": "en-US,en;q=0.9",
+            "Accept-Encoding": "gzip, deflate, br"
+        }
+
+        # NSE cookie initialization
         self.session.get("https://www.nseindia.com", headers=self.headers)
 
     def fetch_full_dataset(self, symbol):
-        url = f"https://www.nseindia.com/api/quote-equity?symbol={symbol.replace('.NS','')}"
-        r = self.session.get(url, headers=self.headers)
-        data = r.json()
 
-        price = data["priceInfo"]["lastPrice"]
-        volume = data["securityWiseDP"]["quantityTraded"]
+        symbol_clean = symbol.replace(".NS", "")
+
+        url = f"https://www.nseindia.com/api/quote-equity?symbol={symbol_clean}"
+
+        r = self.session.get(url, headers=self.headers)
+
+        if r.status_code != 200:
+            return {
+                "symbol": symbol,
+                "price": 0,
+                "volume": 0,
+                "timestamp": str(self.timestamp)
+            }
+
+        data = r.json()
 
         return {
             "symbol": symbol,
-            "price": price,
-            "volume": volume,
+            "price": data["priceInfo"]["lastPrice"],
+            "volume": data["securityWiseDP"]["quantityTraded"],
             "timestamp": str(self.timestamp)
         }
-
