74093bf455fa511684b14d5a79a4894430ab0f2d
puneet1582-tech <puneet1582@gmail.com>
2026-02-17 18:14:31 +0000

    Step-34: Multi-stock universe expansion engine added

---
+++ stock_universe_engine.py
@@ -0,0 +1,22 @@
+# STOCK UNIVERSE ENGINE
+# Provides scalable stock universe list
+
+def get_stock_universe():
+    """
+    Initial scalable universe (expand later to NIFTY 500)
+    """
+
+    stocks = [
+        "RELIANCE.NS",
+        "TCS.NS",
+        "HDFCBANK.NS",
+        "INFY.NS",
+        "ICICIBANK.NS",
+        "HINDUNILVR.NS",
+        "LT.NS",
+        "ITC.NS",
+        "SBIN.NS",
+        "BHARTIARTL.NS"
+    ]
+
+    return stocks
