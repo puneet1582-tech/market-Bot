1a85aa97aefe5c90a7451d55170b8187cc7a4060
puneet1582-tech <puneet1582@gmail.com>
2026-03-05 16:56:50 +0000

    add logging system

--- run.py
+++ run.py
@@ -1,3 +1,4 @@
+from core.system_logger import log
 from core.master_intelligence_controller import MasterIntelligenceController
 from engines.alert_engine.telegram_alert import send_market_report
 
@@ -6,6 +7,8 @@ def main():
     brain = MasterIntelligenceController()
 
     result = brain.execute()
+log("Brain execution completed")
+
 
     print(result)
 
