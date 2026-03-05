f3a538bf5b332b4979464884f19b98001387087f
puneet1582-tech <puneet1582@gmail.com>
2026-03-05 17:11:46 +0000

    fix logging system

--- core/system_logger.py
+++ core/system_logger.py
@@ -3,11 +3,14 @@ import os
 
 os.makedirs("logs", exist_ok=True)
 
-logging.basicConfig(
-    filename="logs/ultimate_brain.log",
-    level=logging.INFO,
-    format="%(asctime)s | %(levelname)s | %(message)s"
-)
+logger = logging.getLogger("ultimate_brain")
+logger.setLevel(logging.INFO)
+
+file_handler = logging.FileHandler("logs/ultimate_brain.log")
+formatter = logging.Formatter("%(asctime)s | %(levelname)s | %(message)s")
+file_handler.setFormatter(formatter)
+
+logger.addHandler(file_handler)
 
 def log(msg):
-    logging.info(msg)
+    logger.info(msg)
