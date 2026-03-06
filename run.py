e422e1b73304d529ba723b21dd17f243f622fd86
puneet1582-tech <puneet1582@gmail.com>
2026-03-06 04:06:01 +0000

    telegram production env setup

--- run.py
+++ run.py
@@ -1,8 +1,12 @@
+from core.env_loader import load_env
+load_env()
+
 from core.master_intelligence_controller import MasterIntelligenceController
 from engines.alert_engine.telegram_alert import send_market_report
 from core.system_logger import log
 
 def main():
+
     brain = MasterIntelligenceController()
 
     result = brain.execute()
