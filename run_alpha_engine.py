cfcc7d0a4ee54cbf4f6eec55857376fce6d7a931
puneet1582-tech <puneet1582@gmail.com>
2026-03-11 01:41:49 +0000

    Architecture cleanup: removed duplicate signal and alpha engines

--- run_alpha_engine.py
+++
@@ -1,4 +0,0 @@
-from engines.alpha.alpha_discovery_engine import run
-
-if __name__ == "__main__":
-    run()
--- run_signal_fusion.py
+++
@@ -1,4 +0,0 @@
-from engines.signals.signal_fusion_engine import run
-
-if __name__ == "__main__":
-    run()
